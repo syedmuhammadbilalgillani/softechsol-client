d415ea0fbfe84e9e429af8128fa372c8bbc22bd7
Syed Muhammad Bilal Gillani <145171612+syedmuhammadbilalgillani@users.noreply.github.com>
2025-12-27 02:46:28 +0500

    Update ProjectCard component: Integrate STORAGE_URL for image URL handling, ensuring proper loading of images from Cloudinary or local storage, and enhance accessibility with improved alt text management.

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,20 +1,22 @@
 "use client";
-import Link from "next/link";
+import { STORAGE_URL } from "@/constants/url";
 import ImageWrapper from "./image-wrapper";
 import ArrowRight from "./svgs/arrow-right";
 
 const ProjectCard = ({ data }: { data: any }) => {
+  const url = data?.image?.url?.startsWith("https://res.cloudinary.com")
+    ? data?.image?.url
+    : `${STORAGE_URL}${data?.image?.url}`;
   return (
     <article className="mb-6 shadow p-5 rounded-xl w-full">
       <ImageWrapper
-        src={data?.image?.url || "/team/1.png"}
+        src={url}
         alt={data?.image?.altText || "Project Image"}
         quality={100}
         width={500}
         height={500}
         className="rounded-xl min-h-[250px] max-h-[250px] object-cover shadow aspect-video w-full"
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 1200px"
-
       />
 
       <div className="flex justify-between gap-5 items-center mt-2">
